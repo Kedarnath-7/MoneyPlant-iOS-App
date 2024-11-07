f6466c1905146ccc1f2e77f51d9fef4e1afbd413
Kedarnath Nagaradone <126375612+Kedarnath-7@users.noreply.github.com>
2024-11-07 16:21:06 +0530

    jhgh

--- MoneyPlant App/AddRecordCollectionViewController.swift
+++ MoneyPlant App/AddRecordCollectionViewController.swift
@@ -7,8 +7,10 @@
 
 import UIKit
 
-class AddRecordCollectionViewController: UIViewController {
-
+class AddRecordCollectionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var categoriesCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,30 +21,49 @@ class AddRecordCollectionViewController: UIViewController {
         
 
         // Do any additional setup after loading the view.
+        
     }
-
-}
-
-extension AddRecordCollectionViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         1
     }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return categories.count
+        categories.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCell", for: indexPath) as? AddRecordCollectionViewCell else { return UICollectionViewCell() }
+        guard let customCell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCell", for: indexPath) as? AddRecordCollectionViewCell else { return UICollectionViewCell() }
         
-        cell.cateogryNameLabel.text = categories[indexPath.row].name
-        cell.categorySymbolLabel.image = categories[indexPath.row].symbol
+        
+            customCell.cateogryNameLabel.text = categories[indexPath.row].name
+            customCell.categorySymbolLabel.image = categories[indexPath.row].symbol
         
         return UICollectionViewCell()
     }
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        print("Selected Category: \(categories[indexPath.row].name)")
-    }
-    
+
 }
+
+//extension AddRecordCollectionViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+//    func numberOfSections(in collectionView: UICollectionView) -> Int {
+//        1
+//    }
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return categories.count
+//    }
+//    
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        
+//        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCell", for: indexPath) as? AddRecordCollectionViewCell else { return UICollectionViewCell() }
+//        
+//        cell.cateogryNameLabel.text = categories[indexPath.row].name
+//        cell.categorySymbolLabel.image = categories[indexPath.row].symbol
+//        
+//        return UICollectionViewCell()
+//    }
+//    
+//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        print("Selected Category: \(categories[indexPath.row].name)")
+//    }
+//    
+//}
