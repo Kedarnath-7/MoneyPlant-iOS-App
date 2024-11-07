774d1ee241e284deb2dc062a05719b1c6b28c74a
Abishek-cell-08 <ap1199@srmist.edu.in>
2024-11-07 16:44:20 +0530

    Update UserInterfaceState.xcuserstate

---
+++ MoneyPlant App/CollectionViewCell1.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionViewCell1.swift
+//  MoneyPlant App
+//
+//  Created by admin15 on 08/11/24.
+//
+
+import UIKit
+
+class CollectionViewCell1: UICollectionViewCell {
+    
+}
---
+++ MoneyPlant App/CollectionViewCell2.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionViewCell2.swift
+//  MoneyPlant App
+//
+//  Created by admin15 on 08/11/24.
+//
+
+import UIKit
+
+class CollectionViewCell2: UICollectionViewCell {
+    
+}
