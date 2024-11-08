76d006496075d516c3705eadf403b8e32cccdbfa
Abishek-cell-08 <ap1199@srmist.edu.in>
2024-11-08 23:36:32 +0530

    ah

--- MoneyPlant App/CollectionViewCell1.swift
+++ MoneyPlant App/CollectionViewCell1.swift
@@ -9,4 +9,26 @@ import UIKit
 
 class CollectionViewCell1: UICollectionViewCell {
     
-}
+    @IBOutlet weak var imageView1: UIImageView!
+
+    @IBOutlet weak var nameLabel1: UILabel!
+    
+    func configure(with image: UIImage) {
+            imageView1.image = image
+        
+        }
+    override func awakeFromNib() {
+            super.awakeFromNib()
+            // Set border color, width, and corner radius for the imageView
+            imageView1.layer.borderColor = UIColor.lightGray.cgColor
+            imageView1.layer.borderWidth = 2.0
+            imageView1.layer.cornerRadius = 8.0
+            imageView1.clipsToBounds = true
+        
+
+        }
+    }
+    
+    
+    
+
