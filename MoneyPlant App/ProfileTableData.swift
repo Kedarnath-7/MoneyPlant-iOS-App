40b9040758cf4f05a385a91704fe7e07db2a5b88
Rohan Soni <rohansoni342ni@gmail.com>
2024-11-12 15:57:34 +0530

    profile

---
+++ MoneyPlant App/ProfileTableData.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileTableViewCell.swift
+//  MoneyPlant App
+//
+//  Created by Rohan on 12/11/24.
+//
+
+import UIKit
+
+struct ProfileTable{
+    var symbol: UIImage
+    var title: String
+}
+
+var profileTable: [ProfileTable] = [
+    ProfileTable(symbol: UIImage(systemName: "wallet.bifold")!, title: "Profile"),
+    ProfileTable(symbol: UIImage(systemName: "gearshape.2")!, title: "Settings"),
+    ProfileTable(symbol: UIImage(systemName: "questionmark")!, title: "Help & Support"),
+    ProfileTable(symbol: UIImage(systemName: "iphone.and.arrow.right.outward")!, title: "Logout")
+]
---
+++ MoneyPlant App/ProfileTableViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ProfileTableViewController.swift
+//  MoneyPlant App
+//
+//  Created by Rohan on 12/11/24.
+//
+
+import UIKit
+
+class ProfileTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+extension ProfileTableViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        profileTable.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "profileCell", for: indexPath) as? ProfileTableViewCell else { return UITableViewCell() }
+        
+        cell.imageOutlet = profileTable[indexPath.row].symbol
+        cell.titleLabel.text = profileTable[indexPath.row].title
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        100
+    }
+    
+}
