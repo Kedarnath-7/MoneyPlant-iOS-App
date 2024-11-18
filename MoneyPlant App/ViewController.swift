dd616e1fa6c58bb87beb0763169f3187305424d5
Kedarnath Nagaradone <126375612+Kedarnath-7@users.noreply.github.com>
2024-11-18 12:32:06 +0530

    scenekit

--- MoneyPlant App/ViewController.swift
+++ MoneyPlant App/ViewController.swift
@@ -9,40 +9,25 @@ import UIKit
 import SceneKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var sceneView: SCNView!
+    
+    var scene: SCNScene!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let scene = SCNScene()
-        
-        let cameraNode = SCNNode()
-        cameraNode.camera = SCNCamera()
-        
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 5)
-        
-        scene.rootNode.addChildNode(cameraNode)
-        
-        let lightNode = SCNNode()
-        lightNode.light = SCNLight()
-        lightNode.light?.type = .omni
-        lightNode.position = SCNVector3(x: 0, y: 10, z: 0)
-        
-        scene.rootNode.addChildNode(lightNode)
-        
-       let stars = SCNParticleSystem(named: "stars.swift", inDirectory: nil)!
-        scene.rootNode.addParticleSystem(stars)
-        
-        let earthNode = EarthNode()
-        scene.rootNode.addChildNode(earthNode)
-        
-        let sceneView = self.view as? SCNView
-        sceneView?.scene = scene
-        
-        sceneView?.showsStatistics = true
-        sceneView?.backgroundColor = .black
-        sceneView?.allowsCameraControl = true
+        setupScene()
         
     }
-
-
+    
+    func setupScene() {
+        sceneView = self.view as? SCNView
+        sceneView.allowsCameraControl = true
+        scene = SCNScene(named: "MainScene.scn")
+        sceneView.scene = scene
+        
+    }
+    
+    
 }
