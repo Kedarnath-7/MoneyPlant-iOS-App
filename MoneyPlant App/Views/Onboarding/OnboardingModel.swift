81171e0d4c0af6ec57f0230eb55859abe778c871
Kedarnath Nagaradone <126375612+Kedarnath-7@users.noreply.github.com>
2024-11-19 11:32:18 +0530

    onboarding

---
+++ MoneyPlant App/Views/Onboarding/OnboardingModel.swift
@@ -0,0 +1,14 @@
+//
+//  OnboardingModel.swift
+//  MoneyPlant App
+//
+//  Created by admin86 on 19/11/24.
+//
+
+import UIKit
+
+struct OnboardingSlide{
+    let title: String
+    let description: String
+    let image: UIImage
+}
---
+++ MoneyPlant App/Views/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,34 @@
+//
+//  OnboardingViewController.swift
+//  MoneyPlant App
+//
+//  Created by admin86 on 19/11/24.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    
+    var slides: [OnboardingSlide] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func continueButtonClicked(_ sender: UIButton) {
+    }
+}
